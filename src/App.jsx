8b4775a184eceb643f912f026f1dee68efb7ddb4
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-12-24 11:11:52 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import Circles from "./Circles.jsx";
+
+function App() {
+  return (
+    <>
+      <Circles />
+      <main className="main">
+        <div className="title">
+          <h1 className="main-title">FREITX</h1>
+          <h2>екосистема вантажних перевезень</h2>
+        </div>
+        <div className="images">
+          <img
+            className="image"
+            src="/images/first_image.webp"
+            alt="First image"
+          />
+          <img
+            className="image"
+            src="/images/second_image.webp"
+            alt="Second image"
+          />
+        </div>
+        <section className="features-section">
+          <ul className="features-list">
+            <li className="features-list-item">
+              <h2 className="subtitle">Надійно</h2>
+              <div className="description">
+                Обирай верифікованого конрагента за рейтингом
+              </div>
+            </li>
+            <li className="features-list-item">
+              <h2 className="subtitle">Зручно</h2>
+              <ul className="features-inner-list">
+                <li className="dotted">Чати</li>
+                <li className="dotted">Документообіг</li>
+                <li className="dotted">Фінансовий кабінет</li>
+                <li className="dotted">Мобільний і GPS-трекінг</li>
+              </ul>
+            </li>
+            <li className="features-list-item">
+              <h2 className="subtitle">Вигідно</h2>
+              <ul className="features-inner-list">
+                <li className="dotted">Інтеграції з великими компаніями</li>
+                <li className="dotted">Бонуси</li>
+                <li className="dotted">Партнерства</li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Circles.jsx
@@ -0,0 +1,12 @@
+const Circles = () => {
+  return (
+    <>
+      <div className="circle top-left"></div>
+      <div className="circle top-right"></div>
+      <div className="circle bottom-left"></div>
+      <div className="circle bottom-right"></div>
+    </>
+  );
+};
+
+export default Circles;
